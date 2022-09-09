738d2bb1b0cc87bd578c9a8a553e98f521d5f7e5
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2022-09-08 21:07:03 -0300

    Iterative DFS, Best First, Map of Parents and Return Visited

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import search
 from graph import Graph
 
 
+# TODO: Timing and profiling
+# TODO: Better path visualization
+
+
 def main():
     vertices = int(input("Vertices:  "))
     neighbors = int(input("Neighbors: "))
 
-    print('-' * 20, end = "\n\n")
+    print()
 
 
     graph = Graph(vertices, neighbors)
@@ -16,15 +20,14 @@ def main():
     print("Edges\n" + '\n'.join(f"{i}: {adj}" for i, adj in enumerate(graph.adj)), end = "\n\n")
 
 
-    path, dist = search.dfs(graph, 0, vertices - 1)
+    path, dist, _ = search.dfs(graph, 0, vertices - 1)
     print(f"DFS: {path} ({dist or 0.0 :.2f})")
 
-    path, dist = search.bfs(graph, 0, vertices - 1)
+    path, dist, _ = search.bfs(graph, 0, vertices - 1)
     print(f"BFS: {path} ({dist or 0.0 :.2f})")
 
-
-    # TODO: Timing and profiling
-    # TODO: Better path visualization
+    path, dist, _ = search.bestfirst(graph, 0, vertices - 1)
+    print(f"Best First: {path} ({dist or 0.0 :.2f})")
 
 
 if __name__ == "__main__":
--- search.py
+++ search.py
@@ -1,52 +1,85 @@
+import heapq
+
 from graph import Graph
-from typing import Optional
 from collections import deque
 
 
+def traceback(parents: dict[int, int], dest: int):
+    """Returns the path taken to dest"""
+    path = []
+    curr: int | None = dest
+
+    while curr is not None:
+        path.append(curr)
+        curr = parents.get(curr)
+
+    path.reverse()
+    return path
+
+
 def dfs(graph: Graph, src: int, dest: int):
-    path: list[int] = []
+    parents: dict[int, int] = {}
     visited: set[int] = set()
+    stack = [(src, 0.0)]
 
-    def recursive(src: int, acc = 0.0) -> Optional[float]:
-        path.append(src)
-        visited.add(src)
+    while stack:
+        curr, dist = stack.pop()
 
-        if src == dest:
-            return acc
+        if curr == dest:
+            return traceback(parents, dest), dist, visited
+        if curr in visited:
+            continue
 
-        for neighbor, weight in graph.adj[src]:
-            if neighbor not in visited and (dist := recursive(neighbor, acc + weight)) is not None:
-                return dist
+        visited.add(curr)
 
-        # Dead end path
-        path.pop()
-        return None
+        for neighbor, weight in graph[curr]:
+            if neighbor not in visited:
+                stack.append((neighbor, dist + weight))
+                parents[neighbor] = curr
 
-    return path, recursive(src)
+    return [], None, visited
 
 
 def bfs(graph: Graph, src: int, dest: int):
+    parents: dict[int, int] = {}
     visited: set[int] = set()
 
-    queue: deque[tuple[list[int], float]] = deque()
-    queue.append(([src], 0.0))
+    queue: deque[tuple[int, float]] = deque()
+    queue.append((src, 0.0))
 
     while queue:
-        path, dist = queue.popleft()
-        visited.add(path[-1])
+        curr, dist = queue.popleft()
+        visited.add(curr)
 
-        if path[-1] == dest:
-            return path, dist
+        if curr == dest:
+            return traceback(parents, dest), dist, visited
 
-        for neighbor, weight in graph.adj[path[-1]]:
+        for neighbor, weight in graph[curr]:
             if neighbor not in visited:
-                queue.append((path + [neighbor], dist + weight))
+                queue.append((neighbor, dist + weight))
+                parents[neighbor] = curr
 
-    return [], None
+    return [], None, visited
 
 
 def bestfirst(graph: Graph, src: int, dest: int):
-    return NotImplemented
+    parents: dict[int, int] = {}
+    visited: set[int] = set()
+    queue = [(0.0, src)]
+
+    while queue:
+        dist, curr = heapq.heappop(queue)
+        visited.add(curr)
+
+        if curr == dest:
+            return traceback(parents, dest), dist, visited
+
+        for neighbor, weight in graph[curr]:
+            if neighbor not in visited:
+                heapq.heappush(queue, (dist + weight, neighbor))
+                parents[neighbor] = curr
+
+    return [], None, visited
 
 
 def dijkstra(graph: Graph, src: int, dest: int):
